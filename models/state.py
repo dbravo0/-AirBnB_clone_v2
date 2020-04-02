71166d73e2118108890dd6f61029c59e39e6ea44
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-02 14:35:17 -0500

    [Fix Check]

--- models/state.py
+++ models/state.py
@@ -18,7 +18,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     if (env_storage == 'db'):
-        cities = relationship("City", backref="state",
+        cities = relationship('City', backref="state",
                               cascade="all, delete, delete-orphan")
     else:
         @property
