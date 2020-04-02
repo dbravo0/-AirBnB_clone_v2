abdc5cf9a3ddbf98267b3a4804b3e9500a75381d
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-02 17:03:40 -0500

    [Fix]Inhrents Base

--- models/review.py
+++ models/review.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """This is the class for Review
     Attributes:
         place_id: place id
