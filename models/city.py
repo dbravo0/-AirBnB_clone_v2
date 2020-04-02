c8f99f2bbc33632801d25c7ae6eecb340c6ae243
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-02 15:48:21 -0500

    [ADD]RelationShip Task 8

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, MetaData
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
@@ -14,3 +15,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship('Place', backref="cities",
+                          cascade="all, delete, delete-orphan")
