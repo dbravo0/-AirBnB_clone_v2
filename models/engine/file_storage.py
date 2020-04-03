9bb8089ccd148819f24703f3799ced5bf08c017f
David <stevenbravobel@gmail.com>
2020-04-03 00:02:21 +0000

    [Fix]

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,11 +20,17 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
+        if (cls is not None):
+            cls_object = {}
+            for key, value in self.__objects.items():
+                if (type(value) is cls):
+                    cls_object[key] = value
+            return cls_object
         return self.__objects
 
     def new(self, obj):
@@ -55,3 +61,13 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """
+            Delete Objects
+        """
+        if (obj):
+            key = ("{}.{}".format(type(obj).__name__, obj.id))
+            if (key in self.__objects):
+                del self.__objects[key]
+            self.save()
