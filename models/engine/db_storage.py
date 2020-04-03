4484f456177f5f29a9606053be283db708ba080f
David <stevenbravobel@gmail.com>
2020-04-03 00:00:40 +0000

    [Fix]

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,75 +1,94 @@
 #!/usr/bin/python3
-"""Module for DBstorage class"""
-from os import getenv
-from sqlalchemy import create_engine, MetaData
+""" This db Storage Class """
+from os import environ
+from models.amenity import Amenity
+from models.base_model import Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
 
 
 class DBStorage():
-    """Class for database storage"""
+    """ Attributes the Class """
     __engine = None
     __session = None
 
     def __init__(self):
-        user = getenv("HBNB_MYSQL_USER")
-        passwd = getenv("HBNB_MYSQL_PWD")
-        db = getenv("HBNB_MYSQL_DB")
-        host = getenv("HBNB_MYSQL_HOST")
-        env = getenv("HBNB_ENV")
+        """ Inizializate Class """
+        usersql = environ.get('HBNB_MYSQL_USER')
+        pwdsql = environ.get('HBNB_MYSQL_PWD')
+        hostsql = environ.get('HBNB_MYSQL_HOST')
+        dbsql = environ.get('HBNB_MYSQL_DB')
+        envsql = environ.get('HBNB_ENV')
 
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(user, passwd, host, db),
-                                      pool_pre_ping=True)
+                                      .format(usersql, pwdsql, hostsql, dbsql), pool_pre_ping=True)
 
-        if env == "test":
-            Base.metadata.drop_all(self.__engine)
+        if (envsql == "test"):
+            Base.metadata.drop_all(bind=self.__engine)
 
     def all(self, cls=None):
-        """returns a dictionary"""
-        from models.state import State
-        from models.city import City
-        from models.user import User
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
+        """
+            All in Dict Objects
+        """
+        session = self.__session
+        dicsql = {}
 
-        class_list = [
-            State,
-            City,
-            User,
-            Place,
-            Review,
-            Amenity
-        ]
-        rows = []
-        if cls:
-            rows = self.__session.query(cls)
+        if (not cls):
+            hbn = [User, City, State, Place, Review]
+
         else:
-            for cls in class_list:
-                rows += self.__session.query(cls)
-        return {type(v).__name__ + "." + v.id: v for v in rows}
+            if (type(cls) == str):
+                cls = eval(cls)
+
+            hbn = [cls]
+
+        for x in hbn:
+            sql = session.query(x)
+
+            for i in sql:
+                key = "{}.{}".format(type(i).__name__, i.id)
+                dicsql[key] = i
+
+        return (dicsql)
 
     def new(self, obj):
-        """add object to db"""
-        if obj:
+        """
+            Adding Current Object DataBase
+        """
+        if (obj):
             self.__session.add(obj)
 
     def save(self):
-        """commit changes to db"""
+        """
+            Save Objects
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete obj from db"""
-        if obj:
+        """
+            Delete Session
+        """
+        if (obj):
             self.__session.delete(obj)
-            self.save()
 
     def reload(self):
-        """create all tables in the db"""
+        """
+            The Session Objects
+        """
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        session = scoped_session(Session)
-        self.__session = session()
+        sess = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        sess_2 = scoped_session(sess)
+
+        self.__session = sess_2()
 
     def close(self):
-        """Thread specific storage"""
+        """
+            Close the Session
+        """
         self.__session.close()
