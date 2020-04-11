957f72544f6f9c7381ae8e69be0b69e4af4103dd
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-11 18:04:42 -0500

    [ADD]0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 # Setup Web Static.
-sudo apt-get update -y
-sudo apt-get install nginx -y
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+apt-get update -y
+apt-get install nginx -y
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
 echo "Holberton School" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-content="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n"
-sudo sed -i "38i $content" /etc/nginx/sites-available/default
-sudo service nginx reload
-sudo service nginx restart
+rm -rf /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+
+loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n"
+
+sudo sed -i "38i $loc" /etc/nginx/sites-available/default
+service nginx restart
+exit 0
