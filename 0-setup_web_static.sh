18978e5207bfb1bc91e86432dc73b06ed2c7dfa1
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-11 18:02:42 -0500

    [ADD]0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,15 +4,7 @@ sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-content="\
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>"
-echo "$content" >> /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 content="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n"
