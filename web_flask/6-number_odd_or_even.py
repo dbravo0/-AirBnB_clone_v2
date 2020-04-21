4fce2d78f9efee590a62acb915b2f69d5c0e67a7
David <stevenbravobel@gmail.com>
2020-04-21 23:27:50 +0000

    [Feature] files 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def HBNH():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_even_template(n):
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", port=5000)
