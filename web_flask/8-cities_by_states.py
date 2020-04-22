9b408a946aa9d4585af50bcb64f3c1b19851bf8c
David <stevenbravobel@gmail.com>
2020-04-22 19:46:30 +0000

    [Feature] files 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from flask import render_template
+from models import storage
+from models import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def closedb(foo):
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def states_list():
+    states = list(storage.all("State").values())
+    states.sort(key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", port=5000)
