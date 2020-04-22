1ab5af88fe46d96c6db7c5fd225862eea9d0d001
David <stevenbravobel@gmail.com>
2020-04-22 19:12:32 +0000

    [Fix] db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -27,9 +27,7 @@ class DBStorage():
         envsql = environ.get('HBNB_ENV')
 
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(usersql, pwdsql,
-                                              hostsql, dbsql),
-                                      pool_pre_ping=True)
+                                      .format(usersql, pwdsql, hostsql, dbsql), pool_pre_ping=True)
 
         if (envsql == "test"):
             Base.metadata.drop_all(bind=self.__engine)
