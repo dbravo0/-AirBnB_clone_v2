2d0842c2428685838104fa0e2eb48df89a6f62bf
David <stevenbravobel@gmail.com>
2020-04-23 03:26:24 +0000

    [Fix] file 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,22 +2,26 @@
 from flask import Flask, render_template
 from models import storage
 from models import State
+from models import City
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def closedb(foo):
+def teardown_appcontext(error):
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def states_list():
-    states = list(storage.all("State").values())
-    states.sort(key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=states)
+def cities_by_states():
+    states = storage.all(State).values()
+    sort_states = sorted(list(states), key=lambda k: k.name)
+    cities = storage.all(City).values()
+    sort_cities = sorted(list(cities), key=lambda k: k.name)
+    return render_template('8-cities_by_states.html', states=sort_states,
+                           cities=sort_cities)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     storage.reload()
     app.run("0.0.0.0", 5000)
