8e417eee9ddd3b0ca9df464900e8a911ca28db96
David <stevenbravobel@gmail.com>
2020-08-18 04:01:57 +0000

    [Fix] file 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,14 +7,15 @@ import os
 
 
 def do_pack():
-    """Funtion do_pack"""
-    pathdir = "versions/"
-    namefile = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
-    if not os.path.exists(pathdir):
-        os.mkdir(pathdir)
-    with tarfile.open(pathdir + namefile, "w:gz") as tar:
+    filename = "web_static_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".tgz"
+
+    if not os.path.exists("versions/"):
+        os.mkdir("versions/")
+
+    with tarfile.open("versions/" + filename, "w:gz") as tar:
         tar.add("web_static", arcname=os.path.basename("web_static"))
-    if os.path.exists(pathdir + namefile):
-        return pathdir + namefile
-    else:
-        return None
+
+    if os.path.exists("versions/" + filename):
+        return "versions/" + filename
+
+    return None
